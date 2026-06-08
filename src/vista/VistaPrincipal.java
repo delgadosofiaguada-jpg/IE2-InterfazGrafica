905a7d6cc3b71af09703490eaee6de98adac2270
Martina Roldan <Alumno@PC20>
2026-06-08 11:24:26 -0300

    Cree rama y package vista

---
+++ src/vista/VistaPrincipal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+import controlador.Controlador;
+import modelo.InscripcionMateria;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alumno
+ */
+public class VistaPrincipal extends JFrame 
+{
+    
+    
+}
