c9f5a79840677a392eb506440967d13c415a4af5
astudillloguadalupe <astudillloguadalupe@gmail.com>
2026-06-08 17:25:45 -0300

    feat: cree estructura inicial de Controlador

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import dao.InscripcionDAO;
+import modelo.Estudiante;
+import modelo.InscripcionMateria;
+
+/**
+ *
+ * @author guada
+ */
+public class Controlador {
+    private Estudiante estudiante;
+    private InscripcionDAO dao;
+            
+   public Controlador(){
+       
+      dao = new InscripcionDAO();
+      estudiante = new Estudiante ( 
+       
+       "Sofia",
+        "1,2,3,4",
+        "Sistemas",
+          2023
+      ); 
+      for (InscripcionMateria i : dao.cargar()){
+          estudiante.getMaterias().add(i);
+      }
+   }
+   
+}
